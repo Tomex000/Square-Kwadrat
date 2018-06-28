01de84e82defd3a62452030bf432274dc1178d91
Tomex000 <39013545+Tomex000@users.noreply.github.com>
2018-06-29 00:27:54 +0200

    Add files via upload

---
+++ Kwadrat/Kwadrat/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Kwadrat
+{
+    class Kwadrat
+    {
+        static void Main(string[] args)
+        {
+            int a, b = 0;
+            for (a = 1; a <= 11; a++)
+            {
+                for (b = 1; b <= 11; b++)
+
+                {
+                    if (a == 1 || a == 11)
+                    {
+
+                        if (b >= 1 && b <= 11)
+                        {
+                            Console.Write("-");
+                        }
+                        else if (b == 1 || b == 11)
+                        {
+                            Console.Write("|");
+                        }
+                    }
+                    else if (b == 1 || b == 11)
+                    {
+                        Console.Write("|");
+                    }
+                    else if (a == 6 && b == 6)
+                        Console.Write("x");
+                    else
+                        Console.Write(" ");
+
+
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
